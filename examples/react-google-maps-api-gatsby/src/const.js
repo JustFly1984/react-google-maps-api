d4bd4d128f015a2950be1e9088333f8bf5e0d92f
Alexey Lyakhov <justfly1984@gmail.com>
2018-12-08 19:37:35 -0400

    added input for google api key in examples

--- examples/react-google-maps-api-gatsby/src/const.js
+++
@@ -1,4 +0,0 @@
-// Past your GoogleMaps API key here
-// You can obtain the API key here:
-// https://developers.google.com/maps/documentation/javascript/get-api-key
-export const googleMapsApiKey = 'AIzaSyBhXc9eNsExvk05UPz-U14kgVvq5q0FbEQ'
