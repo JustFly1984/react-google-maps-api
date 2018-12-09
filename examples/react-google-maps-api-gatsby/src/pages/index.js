c2f3e66b58c658d5a85c6405d9fe5cefd6a89c99
Alexey Lyakhov <justfly1984@gmail.com>
2018-12-08 20:03:44 -0400

    example toggle checkboxes

--- examples/react-google-maps-api-gatsby/src/pages/index.js
+++ examples/react-google-maps-api-gatsby/src/pages/index.js
@@ -27,11 +27,6 @@ const mapHeaderStyle = {
   marginBottom: '1.5rem'
 }
 
-// const center = {
-//   lat: -34.397,
-//   lng: 150.644
-// }
-
 const loadingStyle = {
   height: `100%`,
   backgroundColor: '#023456'
@@ -60,8 +55,16 @@ const loaderId = uuid()
 
 class IndexPage extends Component {
   state = {
-    checked: true,
-    googleMapsApiKey: ''
+    checked: false,
+    googleMapsApiKey: '',
+    fusion: false,
+    directions: false,
+    heatmap: false,
+    traffic: false,
+    shapes: false,
+    drawing: false,
+    bicycling: false,
+    ground: false
   }
 
   onChange = ({ target: { checked } }) => {
@@ -76,6 +79,70 @@ class IndexPage extends Component {
     this.setState(
       () => ({
         googleMapsApiKey: value
+      })
+    )
+  }
+
+  onFusionChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        fusion: checked
+      })
+    )
+  }
+
+  onDirectionsChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        directions: checked
+      })
+    )
+  }
+
+  onHeatmapChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        heatmap: checked
+      })
+    )
+  }
+
+  onTrafficChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        traffic: checked
+      })
+    )
+  }
+
+  onShapesChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        shapes: checked
+      })
+    )
+  }
+
+  onDrawingChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        drawing: checked
+      })
+    )
+  }
+
+  onBicyclingChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        bicycling: checked
+      })
+    )
+  }
+
+  onGroundChange = ({ target: { checked } }) => {
+    this.setState(
+      () => ({
+        ground: checked
       })
     )
   }
@@ -112,6 +179,128 @@ class IndexPage extends Component {
         <label htmlFor='toggle-script'>{`Toggle <LoadScript />`}</label>
       </div>
 
+      <div>
+        <div>
+          <label
+            htmlFor='fusion'
+          >
+            Fusion Layer Example
+          </label>
+          {` `}
+          <input
+            id='fusion'
+            type='checkbox'
+            onChange={this.onFusionChange}
+            value={this.state.fusion}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='directions'
+          >
+            Directions Example
+          </label>
+          {` `}
+          <input
+            id='directions'
+            type='checkbox'
+            onChange={this.onDirectionsChange}
+            value={this.state.directions}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='heatmap'
+          >
+            Heatmap Layer Example
+          </label>
+          {` `}
+          <input
+            id='heatmap'
+            type='checkbox'
+            onChange={this.onHeatmapChange}
+            value={this.state.heatmap}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='traffic'
+          >
+            Traffic Layer Example
+          </label>
+          {` `}
+          <input
+            id='traffic'
+            type='checkbox'
+            onChange={this.onTrafficChange}
+            value={this.state.traffic}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='shapes'
+          >
+            Shapes Example
+          </label>
+          {` `}
+          <input
+            id='shapes'
+            type='checkbox'
+            onChange={this.onShapesChange}
+            value={this.state.shapes}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='drawing'
+          >
+            Drawing Layer Example
+          </label>
+          {` `}
+          <input
+            id='drawing'
+            type='checkbox'
+            onChange={this.onDrawingChange}
+            value={this.state.drawing}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='bicycling'
+          >
+            Bicycling Layer Example
+          </label>
+          {` `}
+          <input
+            id='bicycling'
+            type='checkbox'
+            onChange={this.onBicyclingChange}
+            value={this.state.bicycling}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor='ground'
+          >
+            Ground Layer Example
+          </label>
+          {` `}
+          <input
+            id='ground'
+            type='checkbox'
+            onChange={this.onGroundChange}
+            value={this.state.ground}
+          />
+        </div>
+      </div>
+
       {
         (
           this.state.checked &&
@@ -129,85 +318,117 @@ class IndexPage extends Component {
               libraries={googleMapsLibraries}
               preventGoogleFontsLoading
             >
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Fusion Tables Layer Google Map example
-                </h2>
+              {
+                this.state.fusion && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Fusion Tables Layer Google Map example
+                    </h2>
 
-                <FusionTablesLayerExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <FusionTablesLayerExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Directions Renderer Google Map example
-                </h2>
+              {
+                this.state.directions && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Directions Renderer Google Map example
+                    </h2>
 
-                <DirectionsRendererExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <DirectionsRendererExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Heatmap Layer Google Map example
-                </h2>
+              {
+                this.state.heatmap && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Heatmap Layer Google Map example
+                    </h2>
 
-                <HeatmapLayerExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <HeatmapLayerExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Traffic Layer Google Map example
-                </h2>
+              {
+                this.state.traffic && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Traffic Layer Google Map example
+                    </h2>
 
-                <TrafficExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <TrafficExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Google Map with Shapes
-                </h2>
+              {
+                this.state.shapes && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Google Map with Shapes
+                    </h2>
 
-                <ShapesExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <ShapesExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Google Map with DrawingManager
-                </h2>
+              {
+                this.state.drawing && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Google Map with DrawingManager
+                    </h2>
 
-                <DrawingManagerExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <DrawingManagerExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Google Map with Bicycling Layer
-                </h2>
+              {
+                this.state.bicycling && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Google Map with Bicycling Layer
+                    </h2>
 
-                <BicyclingExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <BicyclingExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
 
-              <div style={mapBoxStyle}>
-                <h2 style={mapHeaderStyle}>
-                  Google Map with Ground Overlay
-                </h2>
+              {
+                this.state.ground && (
+                  <div style={mapBoxStyle}>
+                    <h2 style={mapHeaderStyle}>
+                      Google Map with Ground Overlay
+                    </h2>
 
-                <GroundOverlayExample
-                  styles={shapeExampleStyles}
-                />
-              </div>
+                    <GroundOverlayExample
+                      styles={shapeExampleStyles}
+                    />
+                  </div>
+                )
+              }
             </LoadScript>
           )
           : null
