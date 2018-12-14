7851b1315a6fcb6ff504304663bf0f293d7e343e
Alexey Lyakhov <justfly1984@gmail.com>
2018-12-14 14:36:35 -0400

    fix docs

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,8 @@
 export const imports = {
+  'src/docs/BicyclingLayer.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
+  'src/docs/DirectionsRenderer.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-directions-renderer" */ 'src/docs/DirectionsRenderer.mdx'),
   'src/docs/GoogleMap.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
