46c9310f2e6e0d944d46143055c0bfb13a4692aa
Uri Klar <uriklar@gmail.com>
2018-12-15 23:06:19 +0200

    Added link to docs in readme

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { setConfig, hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
 
-const Root = () => (
-  <Theme hashRouter={undefined} websocketUrl="ws://127.0.0.1:60505" />
-)
+const Root = () => <Theme hashRouter={undefined} />
 
 // TODO: this is temporary until react-hot-loader fix hooks issues
 setConfig({
