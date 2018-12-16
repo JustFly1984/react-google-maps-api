3b804ccf71e3ee211fa4c95c84ec8c68adb5db44
Xali <1515257+xalisys@users.noreply.github.com>
2018-12-16 09:37:02 -0500

    updated docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,12 +3,10 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
   'src/docs/GoogleMap.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
-  'src/docs/GoogleMapPersistedInstance.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map-persisted-instance" */ 'src/docs/GoogleMapPersistedInstance.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
-  'src/docs/Polygon.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polygon" */ 'src/docs/Polygon.mdx'),
+  'src/docs/GoogleMapPersistedInstance.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map-persisted-instance" */ 'src/docs/GoogleMapPersistedInstance.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
