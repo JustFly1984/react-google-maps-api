82515e14fbe9a12a2b669cc4d0d2ba3477406bfa
Xali <1515257+xalisys@users.noreply.github.com>
2018-12-16 17:36:50 -0500

    fix persitance logic, to incorporate into GoogleMap

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,12 +1,10 @@
 export const imports = {
-  'src/docs/BicyclingLayer.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
   'src/docs/GoogleMap.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
+  'src/docs/BicyclingLayer.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
-  'src/docs/GoogleMapPersistedInstance.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map-persisted-instance" */ 'src/docs/GoogleMapPersistedInstance.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
