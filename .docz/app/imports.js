90ee11bdc8286e602e73bdce565f8425ad91525c
Gregory Menezes <gregoryfontenele@gmail.com>
2018-12-18 21:38:19 +1100

    Added polygon example to docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -5,6 +5,8 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
+  'src/docs/Polygon.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polygon" */ 'src/docs/Polygon.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
