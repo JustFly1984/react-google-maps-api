038bb1601dee82d006df62f628ee83e6142e19a1
Alexey Lyakhov <justfly1984@gmail.com>
2019-01-31 20:48:35 -0400

    debug

--- packages/react-google-maps-api-gatsby-example/src/examples/standalone-searchbox-example.js
+++ packages/react-google-maps-api-gatsby-example/src/examples/standalone-searchbox-example.js
@@ -17,20 +17,6 @@ const center = {
   lng: -180
 }
 
-const inputStyle = {
-  boxSizing: `border-box`,
-  border: `1px solid transparent`,
-  width: `240px`,
-  height: `32px`,
-  padding: `0 12px`,
-  borderRadius: `3px`,
-  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-  fontSize: `14px`,
-  outline: `none`,
-  textOverflow: `ellipses`,
-  position: 'absolute'
-}
-
 const StandaloneSearchboxExample = ({ styles }) => (
   <div>
     <GoogleMap
@@ -45,7 +31,22 @@ const StandaloneSearchboxExample = ({ styles }) => (
         <input
           type='text'
           placeholder='Customized your placeholder'
-          style={inputStyle}
+          // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `240px`,
+            height: `32px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+            position: 'absolute',
+            top: '10px',
+            right: '10px'
+          }}
         />
       </StandaloneSearchBox>
     </GoogleMap>
