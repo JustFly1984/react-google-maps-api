5547e1b766581e4fafec78f3edf230a129649a16
Uri Klar <uriklar@gmail.com>
2019-02-11 08:27:02 +0200

    Uncomment StreetViewPanorama (still not working)

--- packages/react-google-maps-api/src/components/streetview/StreetViewPanorama.tsx
+++ packages/react-google-maps-api/src/components/streetview/StreetViewPanorama.tsx
@@ -1,167 +1,142 @@
-// /* global google */
-// TODO:
-// import { PureComponent, Fragment } from 'react'
-// //@ts-ignore
-// import invariant from 'invariant'
+/* global google */
+import { PureComponent } from 'react'
 
-// import {
-//   unregisterEvents,
-//   applyUpdatersToPropsAndRegisterEvents
-// } from '../../utils/helper'
+import { unregisterEvents, applyUpdatersToPropsAndRegisterEvents } from '../../utils/helper'
 
-// import MapContext from '../../map-context'
+import MapContext from '../../map-context'
 
-// import { StreetViewPanoramaPropTypes } from '../../proptypes'
-// import { LatLng } from '../../types'
+import { LatLng } from '../../types'
 
-// const eventMap = {
-//   onCloseClick: 'closeclick',
-//   onPanoChanged: 'pano_changed',
-//   onPositionChanged: 'position_changed',
-//   onPovChanged: 'pov_changed',
-//   onResize: 'resize',
-//   onStatusChanged: 'status_changed',
-//   onVisibleChanged: 'visible_changed',
-//   onZoomChanged: 'zoom_changed',
-// }
+const eventMap = {
+  onCloseClick: 'closeclick',
+  onPanoChanged: 'pano_changed',
+  onPositionChanged: 'position_changed',
+  onPovChanged: 'pov_changed',
+  onResize: 'resize',
+  onStatusChanged: 'status_changed',
+  onVisibleChanged: 'visible_changed',
+  onZoomChanged: 'zoom_changed'
+}
 
-// const updaterMap = {
-//   register (instance: google.maps.StreetViewPanorama, provider: (input: string) => google.maps.StreetViewPanoramaData, options: any) {
-//     // @ts-ignore
-//     instance.registerPanoProvider(provider, options)
-//   },
-//   links (instance: google.maps.StreetViewPanorama, links: google.maps.StreetViewLink[]) {
-//     instance.setLinks(links)
-//   },
-//   motionTracking (instance: google.maps.StreetViewPanorama, motionTracking: boolean) {
-//     instance.setMotionTracking(motionTracking)
-//   },
-//   options (instance: google.maps.StreetViewPanorama, options: google.maps.StreetViewPanoramaOptions) {
-//     instance.setOptions(options)
-//   },
-//   pano (instance: google.maps.StreetViewPanorama, pano: string) {
-//     instance.setPano(pano)
-//   },
-//   position (instance: google.maps.StreetViewPanorama, position: LatLng) {
-//     instance.setPosition(position)
-//   },
-//   pov (instance: google.maps.StreetViewPanorama, pov: google.maps.StreetViewPov) {
-//     instance.setPov(pov)
-//   },
-//   visible (instance: google.maps.StreetViewPanorama, visible: boolean) {
-//     instance.setVisible(visible)
-//   },
-//   zoom (instance: google.maps.StreetViewPanorama, zoom: number) {
-//     instance.setZoom(zoom)
-//   }
-// }
+const updaterMap = {
+  register (
+    instance: google.maps.StreetViewPanorama,
+    provider: (input: string) => google.maps.StreetViewPanoramaData,
+    options: any
+  ) {
+    // @ts-ignore
+    instance.registerPanoProvider(provider, options)
+  },
+  links (instance: google.maps.StreetViewPanorama, links: google.maps.StreetViewLink[]) {
+    instance.setLinks(links)
+  },
+  motionTracking (instance: google.maps.StreetViewPanorama, motionTracking: boolean) {
+    instance.setMotionTracking(motionTracking)
+  },
+  options (
+    instance: google.maps.StreetViewPanorama,
+    options: google.maps.StreetViewPanoramaOptions
+  ) {
+    instance.setOptions(options)
+  },
+  pano (instance: google.maps.StreetViewPanorama, pano: string) {
+    instance.setPano(pano)
+  },
+  position (instance: google.maps.StreetViewPanorama, position: LatLng) {
+    instance.setPosition(position)
+  },
+  pov (instance: google.maps.StreetViewPanorama, pov: google.maps.StreetViewPov) {
+    instance.setPov(pov)
+  },
+  visible (instance: google.maps.StreetViewPanorama, visible: boolean) {
+    instance.setVisible(visible)
+  },
+  zoom (instance: google.maps.StreetViewPanorama, zoom: number) {
+    instance.setZoom(zoom)
+  }
+}
 
-// interface StreetViewPanoramaState {
-//   streetViewPanorama?: google.maps.StreetViewPanorama
-// }
+interface StreetViewPanoramaState {
+  streetViewPanorama?: google.maps.StreetViewPanorama
+}
 
-// interface StreetViewPanoramaProps {
-//   options?: google.maps.StreetViewPanoramaOptions;
-//   onCloseclick?: (event: google.maps.event) => void;
-//   onPanoChanged: () => void;
-//   onPositionChanged: () => void;
-//   onPovChanged: () => void;
-//   onResize: () => void;
-//   onStatusChanged: () => void;
-//   onVisibleChanged: () => void;
-//   onZoomChange: () => void;
-// }
+//prettier-ignore
+interface StreetViewPanoramaProps {
+  containerElement: Element;
+  options?: google.maps.StreetViewPanoramaOptions;
+  onCloseclick?: (event: google.maps.event) => void;
+  onPanoChanged?: () => void;
+  onPositionChanged?: () => void;
+  onPovChanged?: () => void;
+  onResize?: () => void;
+  onStatusChanged?: () => void;
+  onVisibleChanged?: () => void;
+  onZoomChange?: () => void;
+}
 
-// export class StreetViewPanorama extends PureComponent {
-//   static propTypes = StreetViewPanoramaPropTypes
+export class StreetViewPanorama extends PureComponent<
+  StreetViewPanoramaProps,
+  StreetViewPanoramaState
+  > {
+  static contextType = MapContext
 
-//   static contextType = MapContext
+  registeredEvents: google.maps.MapsEventListener[] = []
 
-//   registerEvents = []
+  state: StreetViewPanoramaState = {
+    streetViewPanorama: null
+  }
 
-//   state = {
-//     streetViewPanorama: null
-//   }
+  componentDidMount = () => {
+    const streetViewPanorama = this.context.getStreetView()
+    this.setState(
+      () => ({
+        streetViewPanorama
+      }),
+      () => {}
+    )
+  }
 
-//   constructor (props, context) {
-//     super(props, context)
+  componentDidUpdate = prevProps => {
+    unregisterEvents(this.registeredEvents)
 
-//     invariant(
-//       this.context,
-//       'Did you render <StreetViewPanorama> as a child of <GoogleMapProvider>?'
-//     )
-//   }
+    this.registeredEvents = applyUpdatersToPropsAndRegisterEvents({
+      updaterMap,
+      eventMap,
+      prevProps,
+      nextProps: this.props,
+      instance: this.state.streetViewPanorama
+    })
+  }
 
-//   componentDidMount = () => {
-//     const streetViewPanorama = this.context.getStreetView()
-//     this.setState(
-//       () => ({
-//         streetViewPanorama
-//       }),
-//       () => {
+  componentWillUnmount = () => {
+    unregisterEvents(this.registeredEvents)
 
-//       }
-//     )
-//   }
+    if (this.state.streetViewPanorama) {
+      this.state.streetViewPanorama.setVisible(false)
+    }
+  }
 
-//   componentDidUpdate = prevProps => {
-//     unregisterEvents(this.registeredEvents)
+  render = () => null
 
-//     this.registeredEvents = applyUpdatersToPropsAndRegisterEvents({
-//       updaterMap,
-//       eventMap,
-//       prevProps,
-//       nextProps: this.props,
-//       instance: this.state.streetViewPanorama
-//     })
-//     this.state.streetViewPanorama.setOptions(this.props)
-//   }
+  getLinks = () => this.state.streetViewPanorama.getLinks()
 
-//   componentWillUnmount = () => {
-//     unregisterEvents(this.registeredEvents)
+  getLocation = () => this.state.streetViewPanorama.getLocation()
 
-//     if (this.state.streetViewPanorama) {
-//       this.state.streetViewPanorama.setVisible(false)
-//     }
-//   }
+  getMotionTracking = () => this.state.streetViewPanorama.getMotionTracking()
 
-//   render = () => (
-//     <Fragment>
-//       {
-//         this.props.children
-//       }
-//     </Fragment>
-//   )
+  getPano = () => this.state.streetViewPanorama.getPano()
 
-//   getLinks = () =>
-//     this.state.streetViewPanorama.getLinks()
+  getPhotographerPov = () => this.state.streetViewPanorama.getPhotographerPov()
 
-//   getLocation = () =>
-//     this.state.streetViewPanorama.getLocation()
+  getPosition = () => this.state.streetViewPanorama.getPosition()
 
-//   getMotionTracking = () =>
-//     this.state.streetViewPanorama.getMotionTracking()
+  getPov = () => this.state.streetViewPanorama.getPov()
 
-//   getPano = () =>
-//     this.state.streetViewPanorama.getPano()
+  getStatus = () => this.state.streetViewPanorama.getStatus()
 
-//   getPhotographerPov = () =>
-//     this.state.streetViewPanorama.getPhotographerPov()
+  getVisible = () => this.state.streetViewPanorama.getVisible()
 
-//   getPosition = () =>
-//     this.state.streetViewPanorama.getPosition()
+  getZoom = () => this.state.streetViewPanorama.getZoom()
+}
 
-//   getPov = () =>
-//     this.state.streetViewPanorama.getPov()
-
-//   getStatus = () =>
-//     this.state.streetViewPanorama.getStatus()
-
-//   getVisible = () =>
-//     this.state.streetViewPanorama.getVisible()
-
-//   getZoom = () =>
-//     this.state.streetViewPanorama.getZoom()
-// }
-
-// export default StreetViewPanorama
+export default StreetViewPanorama
