8c1b3a29dc8921a40e2b1fd52e06233f1b2ef275
Uri Klar <uriklar@gmail.com>
2019-02-24 18:02:52 +0200

    added prettier/@typescript-eslint

--- packages/react-google-maps-api/.eslintrc.js
+++ packages/react-google-maps-api/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "prettier/react",
-    "prettier/standard"
+    "prettier/standard",
+    "prettier/@typescript-eslint"
   ],
   plugins: [
     'you-dont-need-lodash-underscore',
