596ac0679a191361f625c52a7a17c5f017dc1645
Uri Klar <uriklar@gmail.com>
2019-03-29 23:57:07 +0300

    uncommit lib files from marker clusterer

--- packages/react-google-maps-api-marker-clusterer/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Clusterer } from './Clusterer';
-export { Cluster } from './Cluster';
-export { ClusterIcon } from './ClusterIcon';
-export { ClusterIconInfo, ClusterIconStyle, MarkerExtended, TCalculator, ClustererOptions } from './types';
-//# sourceMappingURL=index.d.ts.map
--- packages/react-google-maps-api-marker-clusterer/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Clusterer_1 = require("./Clusterer");
-exports.Clusterer = Clusterer_1.Clusterer;
-var Cluster_1 = require("./Cluster");
-exports.Cluster = Cluster_1.Cluster;
-var ClusterIcon_1 = require("./ClusterIcon");
-exports.ClusterIcon = ClusterIcon_1.ClusterIcon;
