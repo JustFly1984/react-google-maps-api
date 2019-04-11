91f14cbe32bcfe2ca42249aaee79a976ec2bdfa1
Alexey Lyakhov <justfly1984@gmail.com>
2019-04-11 04:39:07 -0400

    fix type return on useGoogleMap

--- packages/react-google-maps-api/src/map-context.ts
+++ packages/react-google-maps-api/src/map-context.ts
@@ -3,16 +3,18 @@ import * as invariant from "invariant"
 
 const MapContext = createContext<google.maps.Map | null>(null)
 
-export function useGoogleMap(): google.maps.Map {
+export function useGoogleMap(): google.maps.Map | null {
   invariant(
     !!useContext,
     "useGoogleMap is React hook and requires React version 16.8+",
   )
   const map = useContext(MapContext)
+
   invariant(
     !!map,
     "useGoogleMap needs a GoogleMap available up in the tree"
   )
+
   return map
 }
 
