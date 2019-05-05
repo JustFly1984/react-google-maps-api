7075dc1296086bbb2b456cda378263588d9b5635
Ivan Konev <ivankonev@Ivans-MacBook-Pro.local>
2019-05-04 23:09:49 -0400

    404 Page

--- packages/react-google-maps-api-gatsby-example/src/pages/404.js
+++ packages/react-google-maps-api-gatsby-example/src/pages/404.js
@@ -1,11 +1,26 @@
 // eslint-disable-next-line filenames/match-exported
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Footer from '../components/footer'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className='container'>
+      <div className='card shadow-sm'>
+        <div className='card-body text-center'>
+          <div className='display-1 mt-4 mb-2'>404</div>
+          <h1 className='h4 mb-3'>PAGE NOT FOUND</h1>
+          <p className='text-secondary'>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+          <Link to='/' className='btn btn-primary mt-4 mb-5'>
+            Go To Homepage
+          </Link>
+        </div>
+      </div>
+    </div>
+
+    <Footer />
   </Layout>
 )
 
