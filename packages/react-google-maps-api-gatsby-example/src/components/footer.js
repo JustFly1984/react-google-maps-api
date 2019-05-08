dda720df314fe0dea911baf99484b508887dc388
Ivan Konev <ivankonev@Ivans-MacBook-Pro.local>
2019-05-08 19:03:48 -0400

    Font Color Contrast Update

--- packages/react-google-maps-api-gatsby-example/src/components/footer.js
+++ packages/react-google-maps-api-gatsby-example/src/components/footer.js
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Footer = () => (
   <footer className='footer'>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-lg-10 offset-lg-1'>
-          <span className='text-muted small'>2019 React Google Maps API</span>
-        </div>
-      </div>
-    </div>
+    <div className='text-secondary small'>2019 React Google Maps API</div>
   </footer>
 )
 
