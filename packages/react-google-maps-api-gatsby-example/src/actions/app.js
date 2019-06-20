471f1f3798f77edc1064cece800c58c11fd37df1
Vladimir Marulin <vvmarulin@gmail.com>
2019-06-20 14:41:03 +0700

    Add OverlayView example to react-google-maps-api-gatsby-example

--- packages/react-google-maps-api-gatsby-example/src/actions/app.js
+++ packages/react-google-maps-api-gatsby-example/src/actions/app.js
@@ -14,6 +14,7 @@ import {
   BICYCLING_TOGGLE,
   GROUND_TOGGLE,
   OPTIONS_TOGGLE,
+  OVERLAY_VIEW_TOGGLE,
   STANDALONE_SEARCHBOX_TOGGLE
 } from '../action-types'
 
@@ -30,4 +31,5 @@ export const toggleDrawing = createAction(DRAWING_TOGGLE)
 export const toggleBicycling = createAction(BICYCLING_TOGGLE)
 export const toggleGround = createAction(GROUND_TOGGLE)
 export const toggleOptions = createAction(OPTIONS_TOGGLE)
+export const toggleOverlayView = createAction(OVERLAY_VIEW_TOGGLE)
 export const toggleStandaloneSearchbox = createAction(STANDALONE_SEARCHBOX_TOGGLE)
