1e9ee773a2f4f861009d59f1bc9b25f1156a9203
Cole Marsteller <cgm32547@gmail.com>
2019-08-20 15:19:03 -0700

    add prop description to KmlLayer

--- packages/react-google-maps-api/src/components/kml/KmlLayer.tsx
+++ packages/react-google-maps-api/src/components/kml/KmlLayer.tsx
@@ -33,12 +33,19 @@ interface KmlLayerState {
 
 export interface KmlLayerProps {
   options?: google.maps.KmlLayerOptions;
+  /** The URL of the KML document to display. */
   url?: string;
+  /** The z-index of the layer. */
   zIndex?: number;
+  /** This event is fired when a feature in the layer is clicked. */
   onClick?: (e: google.maps.MouseEvent) => void;
+  /** This event is fired when the KML layers default viewport has changed. */
   onDefaultViewportChanged?: () => void;
+  /** This event is fired when the KML layer has finished loading. At this point it is safe to read the status property to determine if the layer loaded successfully. */
   onStatusChanged?: () => void;
+  /** This callback is called when the kmlLayer instance has loaded. It is called with the kmlLayer instance. */
   onLoad: (kmlLayer: google.maps.KmlLayer) => void;
+  /** This callback is called when the component unmounts. It is called with the kmlLayer instance. */
   onUnmount: (kmlLayer: google.maps.KmlLayer) => void;
 }
 
