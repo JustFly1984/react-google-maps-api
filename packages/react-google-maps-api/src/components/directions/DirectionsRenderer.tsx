00635eca41497af069d278787a172560add7b3d4
Cole Marsteller <cgm32547@gmail.com>
2019-08-20 15:18:15 -0700

    add prop description to DirectionsRenderer

--- packages/react-google-maps-api/src/components/directions/DirectionsRenderer.tsx
+++ packages/react-google-maps-api/src/components/directions/DirectionsRenderer.tsx
@@ -41,11 +41,17 @@ interface DirectionsRendererState {
 
 export interface DirectionsRendererProps {
   options?: google.maps.DirectionsRendererOptions;
+  /** The directions to display on the map and/or in a <div> panel, retrieved as a DirectionsResult object from DirectionsService. */
   directions?: google.maps.DirectionsResult;
+  /** The <div> in which to display the directions steps. */
   panel?: Element;
+  /** The index of the route within the DirectionsResult object. The default value is 0. */
   routeIndex?: number;
+  /** This event is fired when the rendered directions change, either when a new DirectionsResult is set or when the user finishes dragging a change to the directions path. */
   onDirectionsChanged?: () => void;
+  /** This callback is called when the directionsRenderer instance has loaded. It is called with the directionsRenderer instance. */
   onLoad?: (directionsRenderer: google.maps.DirectionsRenderer) => void;
+  /** This callback is called when the component unmounts. It is called with the directionsRenderer instance. */
   onUnmount?: (directionsRenderer: google.maps.DirectionsRenderer) => void;
 }
 
