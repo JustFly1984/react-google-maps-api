e04821d6da23102bceba273f97c564eb2cd01b8b
Gwenaël Gallon <ggallon@users.noreply.github.com>
2019-09-12 23:33:45 +0200

    Add checkbox-transit component

---
+++ packages/react-google-maps-api-gatsby-example/src/components/checkbox-transit.js
@@ -0,0 +1,51 @@
+// eslint-disable-next-line filenames/match-exported
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { toggleTransit } from '../actions/app'
+
+const id = 'transit'
+
+const CheckboxTransit = ({ onChange, value }) => (
+  <div className='custom-control custom-checkbox'>
+    <input
+      id={id}
+      className='custom-control-input'
+      type='checkbox'
+      onChange={onChange}
+      value={value}
+    />
+
+    <label
+      className='custom-control-label'
+      htmlFor={id}
+    >
+      Transit
+    </label>
+  </div>
+)
+
+CheckboxTransit.propTypes = {
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  value: state.getIn(['app', 'transit'])
+})
+
+const mapDispatchToProps = dispatch => ({
+  onChange: ({ target: { checked } }) => {
+    dispatch(
+      toggleTransit({
+        transit: checked
+      })
+    )
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckboxTransit)
