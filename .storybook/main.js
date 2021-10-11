53a2e7589828547aa7616a3f885ab074986e1767
Mischa Radenovic <miroslav.radenovic@gmail.com>
2021-10-11 14:29:17 -0400

    Remove  auto generated stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,6 @@ module.exports = {
     "../packages/react-google-maps-api/**/*.stories.@(js|jsx|ts|tsx)",
     "../packages/react-google-maps-api-gatsby-example/**/*.stories.mdx",
     "../packages/react-google-maps-api-gatsby-example/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
